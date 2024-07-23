faf1f82f59c04170e9a9edf302b477063274b268
zhouzhixuan <zhouzhixuan@chinasie.com>
2024-07-23 10:48:22 +0800

    feat: 没有安装svn命令的情况下调用本地的

--- src/AutoCommit.js
+++ src/AutoCommit.js
@@ -9,7 +9,7 @@ const fileUtils = require('./common/fileUtils');
 class AutoCommit {
   constructor(option = {}) {
     this.option = Object.assign({}, AutoCommit.__option, option);
-    this.svn = fileUtils.getPath('./subversion/bin/svn.exe', process.cwd());
+    this.svnExistCheck();
   }
 
   static __option = {};
@@ -29,7 +29,7 @@ class AutoCommit {
       this.svnAutoCommit(this.execBuilder);
     } catch (err) {
       console.log(chalk.yellow(err.message));
-      execSync('svn cleanup', {
+      execSync(`${this.svn} cleanup`, {
         encoding: 'utf-8',
         stdio: 'inherit',
       });
@@ -40,19 +40,19 @@ class AutoCommit {
 
   async svnAutoCommit(callback) {
     try {
-      execSync('svn update', {
+      execSync(`${this.svn} update`, {
         encoding: 'utf-8',
         stdio: 'inherit',
       });
 
-      execSync('svn add . --force', {
+      execSync(`${this.svn} add . --force`, {
         encoding: 'utf-8',
         stdio: 'inherit',
       });
 
       if (this.comment) {
         this.comment = this.comment === this.commentDefGit ? this.commentDefSvn : this.comment;
-        execSync(`svn commit -m "feat: ${this.comment}"`, {
+        execSync(`${this.svn} commit -m "feat: ${this.comment}"`, {
           encoding: 'utf-8',
           stdio: 'inherit',
         });
@@ -74,7 +74,7 @@ class AutoCommit {
       process.nextTick(async () => {
         const { comment } = await inquirer.prompt(commentQS);
 
-        execSync(`svn commit -m "feat: ${comment}"`, {
+        execSync(`${this.svn} commit -m "feat: ${comment}"`, {
           encoding: 'utf-8',
           stdio: 'inherit',
         });
@@ -145,17 +145,17 @@ class AutoCommit {
   }
 
   svnAddIgnoreExec() {
-    execSync('svn propset svn:ignore -F svnignore.txt .', {
+    execSync(`${this.svn} propset svn:ignore -F svnignore.txt .`, {
       encoding: 'utf-8',
       stdio: 'inherit',
     });
 
-    execSync('svn propset svn:ignore "coverage" tests/unit', {
+    execSync(`${this.svn} propset svn:ignore "coverage" tests/unit`, {
       encoding: 'utf-8',
       stdio: 'inherit',
     });
 
-    execSync('svn up', {
+    execSync(`${this.svn} up`, {
       encoding: 'utf-8',
       stdio: 'inherit',
     });
@@ -165,19 +165,19 @@ class AutoCommit {
     // 该方法可以独立出来作为单独的命令，供添加SVN忽略文件使用
 
     // 全局
-    const addedGlobal = execSync('svn propget svn:global-ignores .', {
+    const addedGlobal = execSync(`${this.svn} propget svn:global-ignores .`, {
       encoding: 'utf-8',
     });
 
     // 本地
     if (!addedGlobal) {
-      execSync('svn propset svn:global-ignores "node_modules .happypack .git" .', {
+      execSync(`${this.svn} propset svn:global-ignores "node_modules .happypack .git" .`, {
         encoding: 'utf-8',
         stdio: 'inherit',
       });
     }
 
-    const added = execSync('svn propget svn:ignore .', {
+    const added = execSync(`${this.svn} propget svn:ignore .`, {
       encoding: 'utf-8',
     });
 
@@ -189,7 +189,7 @@ class AutoCommit {
       } catch (err) {
         const target = fileUtils.getPath('svnignore.txt', process.cwd());
         fileUtils.fsCopy('../../svnignore.txt', target, () => {
-          execSync('svn cleanup', {
+          execSync(`${this.svn} cleanup`, {
             encoding: 'utf-8',
             stdio: 'inherit',
           });
@@ -206,24 +206,38 @@ class AutoCommit {
   }
 
   svnExistCheck() {
-    const exist = execSync('svn --version', {
-      encoding: 'utf-8',
-    });
+    try {
+      const exist = execSync(`${this.svn} --version`, {
+        encoding: 'utf-8',
+      });
 
-    if (exist.toLowerCase().indexOf('svn') > -1) {
-      this.svn = 'svn';
-    } else {
-      this.svn = fileUtils.getPath('./subversion/bin/svn.exe', process.cwd());
+      if (exist.toLowerCase().indexOf('svn') > -1) {
+        this.svn = 'svn';
+      } else {
+        this.svn = fileUtils.getPath('../subversion/bin/svn.exe');
+        const exist = execSync(`${this.svn} --version`, {
+          encoding: 'utf-8',
+        });
+
+        if (exist.toLowerCase().indexOf('svn') <= -1) {
+          throw Error('无法获取【SVN】命令');
+        }
+      }
+    } catch (err) {
+      console.log(chalk.yellow('请安装【SVN】命令！'));
+      console.log(
+        chalk.green('参考文档：https://www.visualsvn.com/server/getting-started/#Installation')
+      );
     }
   }
 
   svnCheck() {
-    const exist = execSync('svn --version', {
+    const exist = execSync(`${this.svn} --version`, {
       encoding: 'utf-8',
     });
 
     if (exist.toLowerCase().indexOf('svn') > -1) {
-      const status = execSync('svn status', {
+      const status = execSync(`${this.svn} status`, {
         encoding: 'utf-8',
       });
 
@@ -233,7 +247,7 @@ class AutoCommit {
           const statusArr = status.split('!');
           statusArr.forEach((path) => {
             path = path.trim();
-            if (path) execSync(`svn delete ${path}`);
+            if (path) execSync(`${this.svn} delete ${path}`);
           });
         }
 
